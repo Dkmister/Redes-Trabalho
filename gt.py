3f2b4339b36c6976c06d9faf99cc1d0048b96970
Vilmar Neto <vilmar.neto@gmail.com>
2019-06-01 05:49:00 +0000

    Erros corrigidos

--- gt.py
+++ gt.py
@@ -4,13 +4,14 @@ import sys
 client = iperf3.Client()
 client.duration = 1
 client.protocol = 'udp' 
+
+if len(sys.argv)!= 4:
+	print("Usage: python3 gt.py hostname port bandwitdh")
+	sys.exit(1)
+
 # Initialization
 client.server_hostname = str(sys.argv[1])
 client.port = int(sys.argv[2])
 client.bandwitdh = int(sys.argv[3]) * 1024
-
-if len(sys.argv)!= 4:
-	print "Usage: python3 gt.py hostname port bandwitdh"
-	sys.exit(1)
 
 client.run()
