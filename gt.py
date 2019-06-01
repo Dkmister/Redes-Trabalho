13fc0ccecfd973bda6fcac87d7794c4ed1f9396c
Vilmar Neto <vilmar.neto@gmail.com>
2019-06-01 05:30:47 +0000

    Adicionado gt.py

---
+++ gt.py
@@ -0,0 +1,16 @@
+import iperf3
+import sys
+
+client = iperf3.Client()
+client.duration = 1
+client.protocol = 'udp' 
+# Initialization
+client.server_hostname = str(sys.argv[1])
+client.port = int(sys.argv[2])
+client.bandwitdh = int(sys.argv[3]) * 1024
+
+if len(sys.argv)!= 4:
+	print "Usage: python3 gt.py hostname port bandwitdh"
+	sys.exit(1)
+
+client.run()
