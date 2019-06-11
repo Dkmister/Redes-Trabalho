dabb6fa87eb87736f61d99dd9d0bc134c1e7edfa
dkmisterv <vilmar.neto@gmail.com>
2019-06-11 15:05:33 -0300

    Added statistic error of timesleep in python.

--- new-gt.py
+++ new-gt.py
@@ -13,15 +13,17 @@ bandwidth = sys.argv[3]
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print('Sending data to '+destIP+ '@'+ port+' with bandwidth of '+bandwidth+'KBps')
 
-time2sleep = (1./(int(bandwidth))) 
+time2sleep = (1/(int(bandwidth))) 
 b = int(bandwidth)
-if b <= 2000:
-	time2sleep = time2sleep * 0.92
+if b <= 1000:
+	time2sleep = time2sleep * 1
+if b > 1000 and b <= 2000:
+	time2sleep = time2sleep * 0.9
 if b > 2000:
-	time2sleep = time2sleep * 0.6
+	time2sleep = time2sleep * 0.6777
 
 
-print(time2sleep)
+print("Time to sleep is: ",time2sleep,"seconds")
 while True:
 	sock.sendto(bytes(1250),(destIP,int(port)))
 	time.sleep(time2sleep)
