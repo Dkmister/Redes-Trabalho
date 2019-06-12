158bf81d93a83fc2eef6eb31c65b076d24506115
Vilmar Neto <vilmar.neto@gmail.com>
2019-06-12 00:45:18 +0000

    Adicionado Comentarios

--- new-gt.py
+++ new-gt.py
@@ -1,20 +1,23 @@
 import socket
 import time
 import sys
-
+# Indica o modo correto do programa para o usuario
 if len(sys.argv)!=4:
 	print("Usage: python3 new-gt.py destinationIP port bandwidth")
 	sys.exit(1)
-
+# Inicializacao das variaveis
 destIP = sys.argv[1]
 port = sys.argv[2]
 bandwidth = sys.argv[3]
-
+# Inicializacao do datagrama UDP
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print('Sending data to '+destIP+ '@'+ port+' with bandwidth of '+bandwidth+'KBps')
+# Calculo do tempo para atingir a largura de banda em KiBs
+time2sleep = (1/(int(bandwidth)))
 
-time2sleep = (1/(int(bandwidth))) 
 b = int(bandwidth)
+# Aqui eh feito uma porcentagem estatistica por conta do erro do sleep
+# Dividido nos intervalos para gerar uma aproximacao melhor
 if b <= 1000:
 	time2sleep = time2sleep * 1
 if b > 1000 and b <= 2000:
@@ -24,6 +27,9 @@ if b > 2000:
 
 
 print("Time to sleep is: ",time2sleep,"seconds")
+
+# Loop de envio:
+# Envio de datagramas ate parada por CTRL-C
 while True:
 	sock.sendto(bytes(1250),(destIP,int(port)))
 	time.sleep(time2sleep)
